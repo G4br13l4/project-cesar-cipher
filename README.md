a626527e07b13e68c889695559380e76f24f4042
G4br13l4 <gabolf93@gmail.com>
2017-10-31 10:54:31 -0600

    Program functions

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ## README
 
-Pedir a usuario que ingrese una frase
-Corroborar que lo que ingrese el usuario no sea un espacio vacio o numeros
-Obtener los ASCII codes de cada caracter de la frase por medio del método charCodeAt()
-Por medio de un if checar si el indice esta en el rango de las letras minusculas o mayusculas
-Crear función llamada "cipher" para calcular los nuevos indices cifrados de acuerdo con la formula de cifrado de César, la formula cambia dependiendo el rango de letras (mayusculas o minusculas)
-Crear funcion llamada "cipher" para transformar nuevos indices en caracteres con el método String.fromCharCode()
+Pedir a usuario que ingrese una frase y guardar en variable "phrase"
+Corroborar que "phrase" no sea un espacio vacio o numeros, si es así mandar alert que ingrese una frase valida
+Obtener los ASCII codes de cada caracter de la frase por medio del método charCodeAt() respetando los espacios de la frase
+Checar si los ASCII codes estan dentro del rango de las letras minúsculas o mayúsculas por medio de un IF
+Declarar la variable "cipheredCodesAll"
+Calcular los nuevos indices cifrados de acuerdo a la formula de cifrado de César, utilizando los codigos ASCII
+Cambiar la formula de cifrado de Cesar dependiendo el rango de letras (mayúsculas o minúsculas)
+Guardar los nuevos indices cifrados en variable "cipheredCodesAll"
+Crear funcion llamada "cipher" para transformar nuevos indices en caracteres cifrados con el método String.fromCharCode()
--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 //Pedir a usuario que ingrese una frase y guardar en variable "phrase"
 var phrase = prompt("Escribe una frase que quieras cifrar");
-//Corroborar que "phrase" no sea un espacio vacio o numeros, si es así mandar alert que ingrese una frase valida
 
-if(phrase ==="")  					                  {
+//Verificar que "phrase" no sea un espacio vacio, si es así mandar alert que indique "frase invalida"
+if(phrase ==="")  					                  	{
 	    alert("Frase invalida");
+//Verificar que "phrase" no sea un número, si es así mandar alert que indique "frase invalida"	    
+} else                                        			{
+		for(var i= 0; i < phrase.length; i++) 		{
+		  var validation = !isNaN(phrase[i]);
+		}
+		  if(validation === true)             {	
+		    alert("Frase invalida");
+		  }else if(validation=== false)       {
+		    alert("Frase valida");
+		  }
 }
 
 //Obtener los ASCII codes de cada caracter de la frase por medio del método charCodeAt() respetando los espacios de la frase
@@ -14,30 +24,37 @@ var result = phrase.charCodeAt(i);
 asciiCodes.push(result);
 }
 console.log(asciiCodes);
-/*Checar si los ASCII codes estan dentro del rango de las letras minúsculas o mayúsculas por medio de un IF
-Calcular los nuevos indices cifrados de acuerdo a la formula de cifrado de César, utilizando los codigos ASCII
-Cambiar la formula de cifrado de Cesar dependiendo el rango de letras (mayúsculas o minúsculas)
-*/
-var cipheredCodesAll= {};
-for(var j= 0; j < asciiCodes.length; j++) {
-	if( asciiCodes[j] > 64 && asciiCodes[j] < 92) {        //mayusculas
+
+//Guardar ASCII Codes en objeto "asciiCodes"
+//Checar si los ASCII codes estan dentro del rango de las letras minúsculas o mayúsculas por medio de un IF
+//Calcular los nuevos indices cifrados de acuerdo a la formula de cifrado de César, utilizando los codigos ASCII
+//Cambiar la formula de cifrado de Cesar dependiendo el rango de letras (mayúsculas o minúsculas)
+
+var cipheredCodesAll= [];
+for(var j= 0; j < asciiCodes.length; j++) 								{
+	if( asciiCodes[j] > 64 && asciiCodes[j] < 92) 					{//mayusculas
 	  	var cipheredCodes = ((asciiCodes[j] - 65 + 33) % 26 + 65);
 	    cipheredCodesAll.push(cipheredCodes);
 
-	}else if(asciiCodes[j] > 96 && asciiCodes[j] < 124) {  //minusculas
+	}else if(asciiCodes[j] > 96 && asciiCodes[j] < 124) 			{//minusculas
 		cipheredCodes = ((asciiCodes[j] - 97 + 33) % 26 + 97);
-	 	cipheredCodesAll.push("cipheredCodes");
+	 	cipheredCodesAll.push(cipheredCodes);
 	}
 }
 console.log(cipheredCodesAll);
+
 //Guardar los nuevos indices cifrados en variable "cipheredCodesAll"
+
 //Crear funcion llamada "cipher" para transformar nuevos indices en caracteres cifrados con el método String.fromCharCode()
 
-/*
-var newPhrase = "";
+var newPhrase = [];
 
-for ( var k = 0; k < cipheredCodesAll.length; k++) {
-		var cipheredCharacter = String.fromCharCode[k];
-		console.log(cipheredCharacter);
+for (var k = 0; k < cipheredCodesAll.length; k++) 		{
+		var cipheredCodes =String.fromCharCode(cipheredCodesAll[k]);
+		newPhrase.push(cipheredCodes); 
 	}
-	*/
+
+//Convertir "newPhrase" cifrada en String sin comas
+newPhrase = newPhrase.join(""); 
+console.log(newPhrase);
+	
